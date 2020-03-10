70c5701eb6dfbf80c38e70cb0bd1c1111d410cc4
eeden01 <mmvaneeden@gmail.com>
2020-03-10 17:45:04 +0100

    all back-end tests work

--- Tests/test.js
+++ Tests/test.js
@@ -4,39 +4,39 @@ const { storeInDB } = require('../scraper/store-headlines');
 const mockData = require('./mockData');
 const HeadlineSchema = require('../server/models/headlines');
 
-//for this test to work first serve ./newspaperStatic.html
-// describe("Test getHeadline", function () {
-//   const laVanGuardia = newspapers.find(obj => obj.newspaper === 'la-vanguardia');
-//   it("getHeadline should return the correct", async function () {
-//     const result = await getHeadline('http://192.168.1.172:5000/newspaperStatic', laVanGuardia.path);
-//     expect(result).toEqual(`El contagio por coronavirus en centros
-//                         de mayores provoca cierres`);
-//   });
-// });
+//for this test to work first serve ./newspaperStatic.html (npx serve Tests) 
+describe("Test getHeadline", function () {
+  const laVanGuardia = newspapers.find(obj => obj.newspaper === 'la-vanguardia');
+  it("getHeadline should return the correct", async function () {
+    const result = await getHeadline('http://192.168.1.172:5000/newspaperStatic', laVanGuardia.path);
+    expect(result).toEqual(`El contagio por coronavirus en centros
+                        de mayores provoca cierres`);
+  });
+});
 
-// describe("Test if all headlines return a headline", function () {
-//   var originalTimeout;
+describe("Test if all headlines return a headline", function () {
+  var originalTimeout;
 
-//   beforeEach(function () {
-//     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-//     jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
-//   });
+  beforeEach(function () {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+  });
 
-//   afterEach(function () {
-//     jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-//   });
+  afterEach(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
 
-//   it("all headline should contain a non-empty string", async function () {
-//     const result = await fetchHeadlines();
-//     let empty = null;
-//     for (headline in result.headlines) {
-//       if (result.headlines[headline].headline === '' || undefined) {
-//         empty = headline;
-//       }
-//     }
-//     expect(empty).toBe(null);
-//   });
-// });
+  it("all headline should contain a non-empty string", async function () {
+    const result = await fetchHeadlines(newspapers);
+    let empty = null;
+    for (headline in result.headlines) {
+      if (result.headlines[headline].headline === '' || undefined) {
+        empty = headline;
+      }
+    }
+    expect(empty).toBe(null);
+  });
+});
 
 describe("Test for store-headLines", function () {
   let inDatabase = false;
