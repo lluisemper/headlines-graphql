d368af6cdbbc9bd7072c42a70bbb5d0aa1372f65
lluisemper <lluisemper@gmail.com>
2020-03-12 13:21:40 +0100

    heroku

--- server/configAuth.js
+++ server/configAuth.js
@@ -19,7 +19,7 @@ passport.deserializeUser(function (obj, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK
+  callbackURL: process.env.PORT + '/auth/google/callback'
 },
 
 
