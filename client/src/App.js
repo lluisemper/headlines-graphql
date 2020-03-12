9d53b656ad04fc8450d23e7308e36658d7ccc12d
lluisemper <lluisemper@gmail.com>
2020-03-12 20:01:39 +0100

    f

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,7 @@ function App () {
         <div className="login">
           <div className="breakingSmall"></div>
           <div className="login-text">
+            <h1>foooooooooooooooooo</h1>
             <a href= {process.env.URL + "/auth/google/banana"}>login</a>
           </div>
         </div>}
