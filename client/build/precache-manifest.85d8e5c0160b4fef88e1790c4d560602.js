7c710b0579722150480767b9d5d55a9e30059c83
lluisemper <lluisemper@gmail.com>
2020-03-12 13:05:06 +0100

    add build

---
+++ client/build/precache-manifest.85d8e5c0160b4fef88e1790c4d560602.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5834cee974224b09a36ddcb76cc0ac2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8925512e5814b752454f",
+    "url": "/static/css/main.5cec362d.chunk.css"
+  },
+  {
+    "revision": "f6b2137be27a331f7ca3",
+    "url": "/static/js/2.9fcbd1f4.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/static/js/2.9fcbd1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8925512e5814b752454f",
+    "url": "/static/js/main.e8bc5411.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "74cc15d9d802fe5d33737e59bb11b52f",
+    "url": "/static/media/breaking.74cc15d9.png"
+  },
+  {
+    "revision": "31b8104f39ed261f33eb57a3f0dea814",
+    "url": "/static/media/newspapers.31b8104f.png"
+  },
+  {
+    "revision": "9f23b41803daef6da65d6773af11cc0e",
+    "url": "/static/media/pngwave.9f23b418.png"
+  }
+]);
